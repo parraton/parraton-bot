5dede9e20e3a1d386c24e8ba2c33541bc66bc205
digberi <orkepuma@gmail.com>
2024-08-14 15:01:47 +0300

    updated contracts addressees, updated adapters

--- src/config/contracts-config.ts
+++ src/config/contracts-config.ts
@@ -1,25 +1,22 @@
 const usdtVault = {
-  vault: "EQBgdJIHy2z4YEo5ssqIOExlGDD61Tn2bIytXtNBj1pW9onM",
-  extraDistributionPool: "EQDpeKN5EPj7sSAToZlLTYppRPNDk2EQghugRKLWlBAjCvOi",
+  vault: 'EQA7Xhpa6ees-6ojaJyVleW_4SCPd6a28AzzTpGxdAyH2DjY',
+  extraDistributionPool: 'EQA3J1fOT97Shq86vaSAd8k3QMphnsUelFkAE38CVxfTp1RJ',
 };
 
-const scaleVault = {
-  vault: "EQCQ1FiAoaZemtGd_5Nz3zJpzBqzG2RLipoBfIvOcioTLAB7",
-  extraDistributionPool: "EQAE6WdY8_uybeL_8jN1nlV-4ixTbUVbAKx--pUjs2mUQL79",
-};
-
-const notVault = {
-  vault: "EQDRdHx-8fC6aW6lSlqAYwOlYo5L5nM8PNChwjxWaQHOdDVx",
-  extraDistributionPool: "EQDpkTnKu_fX_ZRotAVsA9yZTfFd35LBtLWfhO6f1Kp2-b1Z",
-};
-
-const isDev = process.env.NODE_ENV === "development";
+// const scaleVault = {
+//   vault: "EQCQ1FiAoaZemtGd_5Nz3zJpzBqzG2RLipoBfIvOcioTLAB7",
+//   extraDistributionPool: "EQAE6WdY8_uybeL_8jN1nlV-4ixTbUVbAKx--pUjs2mUQL79",
+// };
+//
+// const notVault = {
+//   vault: "EQDRdHx-8fC6aW6lSlqAYwOlYo5L5nM8PNChwjxWaQHOdDVx",
+//   extraDistributionPool: "EQDpkTnKu_fX_ZRotAVsA9yZTfFd35LBtLWfhO6f1Kp2-b1Z",
+// };
 
 export const addresses = {
-  dedustFactory: "EQDHcPxlCOSN_s-Vlw53bFpibNyKpZHV6xHhxGAAT_21nCFU",
-  dedustDistributionFactory: "EQC0WJSeustdSo4fI5eRXmxdu8rqyWz9tBwLmX9E94dQlUCv",
-  extraRewardsDistributionFactory:
-    "EQDmtWKElPJLGJfjlN060ztD6-CwM_8HFNIFH507qiza-x6H",
-  vaultFactory: "EQC3Mcj8A7V3zrmPnOUM3ylpty8Tq2kVIhBWj4cs2rAHnHRK",
-  vaults: isDev ? [usdtVault]: [usdtVault, scaleVault, notVault],
+  dedustFactory: 'EQDHcPxlCOSN_s-Vlw53bFpibNyKpZHV6xHhxGAAT_21nCFU',
+  dedustDistributionFactory: 'EQC0WJSeustdSo4fI5eRXmxdu8rqyWz9tBwLmX9E94dQlUCv',
+  extraRewardsDistributionFactory: 'EQDmtWKElPJLGJfjlN060ztD6-CwM_8HFNIFH507qiza-x6H',
+  vaultFactory: 'EQBilFt7vW_l_6zMv7ABE6iW8v78bLZcJlfxgZ1-RnpI6B7X',
+  vaults:[usdtVault]
 } as const;
--- src/core/contracts/vault.ts
+++ src/core/contracts/vault.ts
@@ -1,13 +1,4 @@
-import {
-  Address,
-  beginCell,
-  Cell,
-  Contract,
-  contractAddress,
-  ContractProvider,
-  Sender,
-  SendMode,
-} from '@ton/core';
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
 
 export type VaultConfig = {
   distributionPoolAddress: Address;
@@ -23,6 +14,15 @@ export type VaultConfig = {
   sharesWalletCode: Cell;
   tempUpgrade: Cell;
 };
+
+export interface VaultFees {
+  depositFee: bigint;
+  withdrawalFee: bigint;
+  reinvestFee: bigint;
+}
+
+export const HOLE_ADDRESS = Address.parse('EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c');
+
 
 export function vaultConfigToCell(config: VaultConfig): Cell {
   return beginCell()
@@ -41,28 +41,32 @@ export function vaultConfigToCell(config: VaultConfig): Cell {
         .endCell(),
     )
     .storeRef(config.sharesWalletCode)
-    .storeRef(config.tempUpgrade)
+    .storeRef(Vault.packInitTempUpgrade())
     .endCell();
 }
 
 export class Vault implements Contract {
-  static readonly MANAGEMENT_FEE_PRECISION = 10_000n;
+  static readonly MANAGEMENT_FEE_PRECISION = 10000n;
   static readonly OPS = {
-    deposit: 0x95_db_9d_39,
-    withdraw: 0xb5_de_5f_9e,
-    init: 0xc6_74_e4_74,
-    reinvest: 0x8_12_d4_e3,
-    complete_reinvest: 0x97_32_80_f5,
-    withdraw_management_fee: 0xef_9e_91_7b,
-    set_strategy_address: 0xa3_d7_61_1f,
-    set_management_fee_rate: 0xc2_5f_fa_5f,
-    set_is_locked: 0xe2_cd_e0_84,
-    transfer_notification: 0x73_62_d0_9c,
-    internal_transfer: 0x17_8d_45_19,
-    burn_notification: 0x7b_dd_97_de,
-    excesses: 0xd5_32_76_db,
-    transfer_bounce_invalid_request: 0x19_72_7e_a8,
-    transfer: 0xf_8a_7e_a5,
+    deposit: 0x95db9d39,
+    withdraw: 0xb5de5f9e,
+    init: 0xc674e474,
+    reinvest: 0x812d4e3,
+    complete_reinvest: 0x973280f5,
+    withdraw_management_fee: 0xef9e917b,
+    set_strategy_address: 0xa3d7611f,
+    set_management_fee_rate: 0xc25ffa5f,
+    set_is_locked: 0xe2cde084,
+    transfer_notification: 0x7362d09c,
+    internal_transfer: 0x178d4519,
+    burn_notification: 0x7bdd97de,
+    excesses: 0xd53276db,
+    transfer_bounce_invalid_request: 0x19727ea8,
+    transfer: 0xf8a7ea5,
+    refund_rewards: 0xb7959473,
+    init_code_upgrade: 0xdf1e233d,
+    init_admin_upgrade: 0x2fb94384,
+    finalize_upgrades: 0x6378509f,
   };
   static readonly EXIT_CODES = {
     WRONG_OP: 80,
@@ -71,7 +75,7 @@ export class Vault implements Contract {
     INVALID_DEPOSIT_TOKEN: 83,
     INSUFFICIENT_GAS: 84,
     INVALID_CALLER: 85,
-    ZERO_OUTPUT: 86,
+    ZERO_SHARES_BURNT: 86,
     INSUFFICIENT_LP_BALANCE: 87,
     INSUFFICIENT_LP_AMOUNT: 88,
     INSUFFICIENT_REWARDS_BALANCE: 89,
@@ -80,6 +84,10 @@ export class Vault implements Contract {
     INSUFFICIENT_MANAGEMENT_FEE: 92,
     INSUFFICIENT_SHARES_BALANCE: 93,
     MANAGEMENT_FEE_RATE_OUT_OF_BOUNDS: 94,
+    INSUFFICIENT_REINVEST_GAS: 95,
+    INSUFFICIENT_CANCEL_DEPOSIT_GAS: 96,
+    INVALID_DEPOSIT_ADDRESS: 97,
+    WRONG_ADMIN_OP: 801,
   };
 
   constructor(
@@ -105,8 +113,13 @@ export class Vault implements Contract {
     });
   }
 
-  prepareDepositPayload() {
-    return beginCell().storeUint(Vault.OPS.deposit, 32).endCell();
+  prepareDepositPayload(fulfillAddress?: Address, fulfillPayload?: Cell) {
+    let depositPayload = beginCell().storeUint(Vault.OPS.deposit, 32);
+    if (fulfillAddress && fulfillPayload) {
+      depositPayload = depositPayload.storeAddress(fulfillAddress);
+      depositPayload = depositPayload.storeRef(fulfillPayload);
+    }
+    return depositPayload.endCell();
   }
 
   async sendDepositNotification(
@@ -135,6 +148,28 @@ export class Vault implements Contract {
     });
   }
 
+  async sendBurnNotification(
+    provider: ContractProvider,
+    via: Sender,
+    opts: {
+      queryId?: number | bigint;
+      amount: bigint;
+      responseAddress: Address;
+      value: bigint;
+    },
+  ) {
+    return provider.internal(via, {
+      value: opts.value,
+      sendMode: SendMode.PAY_GAS_SEPARATELY,
+      body: beginCell()
+        .storeUint(Vault.OPS.burn_notification, 32)
+        .storeUint(opts.queryId ?? 0, 64)
+        .storeCoins(opts.amount)
+        .storeAddress(opts.responseAddress)
+        .endCell(),
+    });
+  }
+
   async sendReinvest(
     provider: ContractProvider,
     via: Sender,
@@ -142,7 +177,8 @@ export class Vault implements Contract {
       value: bigint;
       totalReward: bigint;
       amountToSwap: bigint;
-      limit: bigint;
+      swapLimit: bigint;
+      depositLimit: bigint;
       tonTargetBalance: bigint;
       depositFee: bigint;
       depositFwdFee: bigint;
@@ -152,27 +188,75 @@ export class Vault implements Contract {
       queryId?: number;
     },
   ) {
+    if (opts.amountToSwap > 0) {
+      return provider.internal(via, {
+        value: opts.value,
+        sendMode: SendMode.PAY_GAS_SEPARATELY,
+        body: beginCell()
+          .storeUint(Vault.OPS.reinvest, 32)
+          .storeUint(opts.queryId ?? 0, 64)
+          .storeCoins(opts.totalReward)
+          .storeCoins(opts.amountToSwap)
+          .storeCoins(opts.swapLimit)
+          .storeUint(opts.deadline, 32)
+          .storeCoins(opts.tonTargetBalance)
+          .storeCoins(opts.jettonTargetBalance)
+          .storeCoins(opts.depositFee)
+          .storeCoins(opts.depositFwdFee)
+          .storeCoins(opts.transferFee)
+          .storeRef(beginCell().storeCoins(opts.depositLimit).endCell())
+          .endCell(),
+      });
+    } else {
+      return provider.internal(via, {
+        value: opts.value,
+        sendMode: SendMode.PAY_GAS_SEPARATELY,
+        body: beginCell()
+          .storeUint(Vault.OPS.reinvest, 32)
+          .storeUint(opts.queryId ?? 0, 64)
+          .storeCoins(opts.totalReward)
+          .storeCoins(opts.amountToSwap)
+          .storeCoins(opts.tonTargetBalance)
+          .storeCoins(opts.jettonTargetBalance)
+          .storeCoins(opts.depositFee)
+          .storeCoins(opts.depositFwdFee)
+          .storeCoins(opts.transferFee)
+          .storeRef(beginCell().storeCoins(opts.depositLimit).endCell())
+          .endCell(),
+      });
+    }
+  }
+
+  async sendRefundRewards(
+    provider: ContractProvider,
+    via: Sender,
+    opts: {
+      value: bigint;
+      queryId?: number;
+    },
+  ) {
     return provider.internal(via, {
       value: opts.value,
       sendMode: SendMode.PAY_GAS_SEPARATELY,
       body: beginCell()
-        .storeUint(Vault.OPS.reinvest, 32)
+        .storeUint(Vault.OPS.refund_rewards, 32)
         .storeUint(opts.queryId ?? 0, 64)
-        .storeCoins(opts.totalReward)
-        .storeRef(
-          beginCell()
-            .storeCoins(opts.amountToSwap)
-            .storeCoins(opts.limit)
-            .storeUint(opts.deadline, 32)
-            .storeCoins(opts.tonTargetBalance)
-            .storeCoins(opts.jettonTargetBalance)
-            .storeCoins(opts.depositFee)
-            .storeCoins(opts.depositFwdFee)
-            .storeCoins(opts.transferFee)
-            .endCell(),
-        )
         .endCell(),
     });
+  }
+
+  static packInitTempUpgrade() {
+    return beginCell().storeUint(0, 64).storeUint(0, 64).storeAddress(HOLE_ADDRESS).storeRef(Cell.EMPTY).endCell();
+  }
+
+  static unpackTempUpgrade(cell: Cell) {
+    const slice = cell.beginParse();
+    return {
+      endCode: slice.loadUint(64),
+      endAdmin: slice.loadUint(64),
+      admin: slice.loadAddress(),
+      code: slice.loadRef(),
+    };
   }
 
   async sendSetStrategyAddress(
@@ -194,7 +278,6 @@ export class Vault implements Contract {
         .endCell(),
     });
   }
-
   async sendWithdrawManagementFee(
     provider: ContractProvider,
     via: Sender,
@@ -216,7 +299,6 @@ export class Vault implements Contract {
         .endCell(),
     });
   }
-
   async sendSetIsLocked(
     provider: ContractProvider,
     via: Sender,
@@ -233,6 +315,63 @@ export class Vault implements Contract {
         .storeUint(Vault.OPS.set_is_locked, 32)
         .storeUint(opts.queryId ?? 0, 64)
         .storeUint(opts.isLocked, 1)
+        .endCell(),
+    });
+  }
+  async sendFinalizeUpgrades(
+    provider: ContractProvider,
+    via: Sender,
+    opts: {
+      value: bigint;
+      queryId?: number;
+    },
+  ) {
+    return provider.internal(via, {
+      value: opts.value,
+      sendMode: SendMode.PAY_GAS_SEPARATELY,
+      body: beginCell()
+        .storeUint(Vault.OPS.finalize_upgrades, 32)
+        .storeUint(opts.queryId ?? 0, 64)
+        .endCell(),
+    });
+  }
+
+  async sendInitCodeUpgrade(
+    provider: ContractProvider,
+    via: Sender,
+    opts: {
+      value: bigint;
+      code: Cell;
+      queryId?: number;
+    },
+  ) {
+    return provider.internal(via, {
+      value: opts.value,
+      sendMode: SendMode.PAY_GAS_SEPARATELY,
+      body: beginCell()
+        .storeUint(Vault.OPS.init_code_upgrade, 32)
+        .storeUint(opts.queryId ?? 0, 64)
+        .storeRef(opts.code)
+        .endCell(),
+    });
+  }
+
+  async sendInitAdminUpgrade(
+    provider: ContractProvider,
+    via: Sender,
+    opts: {
+      value: bigint;
+      admin: Address;
+      queryId?: number;
+    },
+  ) {
+    return provider.internal(via, {
+      value: opts.value,
+      sendMode: SendMode.PAY_GAS_SEPARATELY,
+      body: beginCell()
+        .storeUint(Vault.OPS.init_admin_upgrade, 32)
+        .storeUint(opts.queryId ?? 0, 64)
+        .storeAddress(opts.admin)
         .endCell(),
     });
   }
@@ -303,5 +442,18 @@ export class Vault implements Contract {
       },
     ]);
     return result.stack.readAddress();
+  }
+
+  async getDepositFee(provider: ContractProvider): Promise<bigint> {
+    const result = await provider.get('get_deposit_fee', []);
+    return result.stack.readBigNumber();
+  }
+  async getReinvestFee(provider: ContractProvider): Promise<bigint> {
+    const result = await provider.get('get_reinvest_fee', []);
+    return result.stack.readBigNumber();
+  }
+  async getVaultStorageFee(provider: ContractProvider): Promise<bigint> {
+    const result = await provider.get('get_vault_storage_fee', []);
+    return result.stack.readBigNumber();
   }
 }
