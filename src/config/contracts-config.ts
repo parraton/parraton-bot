f8d1f8334ef9b39a6c45e80dff875217c2c498de
KStasi <nastasiakondaurova@gmail.com>
2024-09-03 11:35:54 +0300

    update addresses

--- src/config/contracts-config.ts
+++ src/config/contracts-config.ts
@@ -1,8 +1,11 @@
-const usdtVault = {
+import { environment } from "./environment";
+
+const usdtVaultTestnet = {
   vault: "EQCczQbuj2Z5tLLt4mYb-kVgt52A9kXbkVFWSuToexwbajV_",
   extraDistributionPool: "EQDWQ59GyQ2qxkC2_kdoOnjwfVv_3kTBA1_GQ3JG9QhkM2sq",
 };
 
+const testnetVaults = [usdtVaultTestnet];
 export const addresses = {
-  vaults: [usdtVault],
+  vaults: environment.NETWORK === "testnet" ? testnetVaults : [],
 } as const;
