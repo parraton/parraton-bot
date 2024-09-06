5338a1abbad5aa82af2eda7e4fd17a53ffe79b27
KStasi <nastasiakondaurova@gmail.com>
2024-09-06 22:30:58 +0300

    update time for test run

--- src/index.ts
+++ src/index.ts
@@ -47,6 +47,6 @@ switch (process.env.NETWORK) {
     schedule(vaultExtraRewardsDistributionSchedule, extraRewardsScenario);
     break;
   default:
-    const reinvestSchedule = "25 19 * * *"; // every day at 19:05
+    const reinvestSchedule = "35 19 * * *"; // every day at 19:05
     schedule(reinvestSchedule, reinvestOnlyScenario);
 }
